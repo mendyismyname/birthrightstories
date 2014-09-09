a2fab6f4d988f343dbe24dc97b6666c444f9c969
Sitch <michael.sitchenko@gmail.com>
2014-09-09 18:03:46 -0400

    hashtag analytics & scraping

--- app/services/hashtag_analytics.rb
+++ app/services/hashtag_analytics.rb
@@ -4,10 +4,9 @@ module HashtagAnalytics
   module_function
 
   def top_performing(tag_name, top_performing_limit = 3)
-    top_performing = Hashtag.where(name: tag_name).first.instagram_medias.group_by(&:instagram_user_id).map do |instagram_user_id, instagram_medias|
+    Hashtag.where(name: tag_name).first.instagram_medias.group_by(&:instagram_user_id).flat_map do |instagram_user_id, instagram_medias|
       instagram_medias.sort_by(&:engagement).last(top_performing_limit)
     end
-    top_performing.flatten
   end
 
   # To Call: 
@@ -18,7 +17,7 @@ module HashtagAnalytics
       
       file_uri = instagram_media.standard_resolution_image_url
       file_ext = file_uri.split('.').last
-      filename = "id_#{instagram_media.id}_username_#{instagram_media.instagram_user.username}.#{file_ext}"
+      filename = "instagram_id_#{instagram_media.instagram_id}_username_#{instagram_media.instagram_user.username}.#{file_ext}"
 
       open([Rails.root,'photoshop_images', filename].join('/'), 'wb') do |file|
         file << open(file_uri).read
--- app/services/hashtag_scraper.rb
+++ app/services/hashtag_scraper.rb
@@ -10,48 +10,24 @@ module HashtagScraper
   def fetch_tag_sequence(tag_name)
     tag = update_hashtag_metadata(tag_name)
 
-
-    # index = 1;
-
     loop do
       tags = scrape tag_name, options ||= {min_tag_id: tag.min_tag_id || 0, max_tag_id: tag.max_tag_id}
       next_url = tags.pagination.next_url
 
       break unless next_url
 
-      options = Hash[next_url.split('?').last.split('&').map {|x| x.split('=')}].symbolize_keys.slice(:min_tag_id, :max_tag_id)
-
-      # puts '----------------------------------------------------'
-      # puts next_url
-      # puts Hash[next_url.split('?').last.split('&').map {|x| x.split('=')}]
-      # puts options
-      # puts '----------------------------------------------------'
-
-
+      options = Hash[next_url.split('?').last.split('&').map {|x| x.split('=')}]
+                  .symbolize_keys
+                  .slice(:min_tag_id, :max_tag_id)
       tag.update_attributes options
-      # index += 1
-      # break if index > 7
-
-      # puts '====================================================='
-      
-      # puts index
-      
-      # puts tags.pagination
-      # puts options.compact
-      # puts '====================================================='
     end
   end
 
   def scrape(tag_name, options={})
     tag = Hashtag.where(name: tag_name).first_or_create
 
-    # options = { 
-    #   min_tag_id: options[:min_tag_id], 
-    #   max_tag_id: options[:max_tag_id]
-    # }
-    
     Instagram.client.tag_recent_media(tag_name, options.compact).each do |media_hash|
-      begin
+      # begin
         InstagramMedia.where(instagram_id: media_hash.id).first_or_initialize do |media|
 
           user = InstagramUser.where(instagram_id: media_hash.user.id).first_or_initialize do |user|
@@ -64,14 +40,13 @@ module HashtagScraper
           media.assign_attributes attrs
           media.instagram_user = user
           media.hashtags << tag
-
-
           media.save
         end
-      rescue
-        puts 'Instagram Scrape Error'
-        puts media_hash
-      end
+      # rescue
+      #   puts 'Instagram Scrape Error'
+      #   puts media_hash
+
+      # end
     end
   end
 
@@ -79,12 +54,6 @@ module HashtagScraper
     Hashtag.find_each do |tag|
       DelayedJob.enque InstagramTagScrapeWorker.new(tag.name, tag.min_tag_id, tag.max_tag_id)
     end
-  end
-
-  private
-
-  def paginated_scraper(tag_name)
-    
   end
 
 end
