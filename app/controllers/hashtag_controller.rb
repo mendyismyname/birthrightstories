7202020d13047c32c0dd621a02081f1ebedf0363
Sitch <michael.sitchenko@gmail.com>
2014-10-14 11:47:15 -0400

    autoexpand fix

--- app/controllers/hashtag_controller.rb
+++ app/controllers/hashtag_controller.rb
@@ -5,12 +5,12 @@ class HashtagController < ApplicationController
   expose(:results)
   expose(:hashtag)
   expose(:current_page)                  { params[:page] }
-  expose(:autoexpand_id)                 { params[:autoexpand_id] }
+  expose(:autoexpand_id)                 { params[:autoexpand_id] || false }
   expose(:instagram_medias)              { hashtag.instagram_medias
                                                   .displayable
                                                   .page(current_page).per(24)
                                                   .includes(:instagram_user) }
-  expose(:selected)                      { [InstagramMedia.find(autoexpand_id)].compact if autoexpand_id && (current_page.blank? || current_page == 1)}
+  expose(:selected)                      { autoexpaned_selection }
   expose(:current_story_count)           { '400,000' }
 
   expose(:other_available_locales) { I18n.available_locales.find_all {|l| l != I18n.locale } }
@@ -22,5 +22,13 @@ class HashtagController < ApplicationController
   # def instagram_medias_hack
   #   InstagramMedia.displayable.order_by_rand.page(current_page).per(60).includes(:instagram_user)
   # end
+
+  def autoexpaned_selection
+    if autoexpand_id && (current_page.blank? || current_page == 1) && !instagram_medias.pluck(:id).include?(autoexpand_id.try(:to_i))
+      [InstagramMedia.find(autoexpand_id)].compact
+    else
+      []
+    end
+  end
 
 end
